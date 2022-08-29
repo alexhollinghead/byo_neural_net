e2d971fdb6a7fb1cc888d9bedce10364368d7b58
Alex Hollinghead <alex@vpn12-client-172-20-214-25.princeton.edu>
2022-08-29 15:24:08 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# neural network class definition
+class neuralNetwork:
+
+    # initialise the neural network
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.innodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        # learning rate
+        self.lr = learningrate
+        pass
+
+    #train the neural network
+    def train():
+        pass
+
+    # query the neural network
+    def query():
+        pass
